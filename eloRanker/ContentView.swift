36b56ca92b3f8f41185fccdeec0b71b4d0094bb6
rodrisy <rodrigo.s.yuste@gmail.com>
2025-04-30 17:10:29 -0400

    Initial Commit

---
+++ eloRanker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eloRanker
+//
+//  Created by Rodrigo Sánchez Yuste on 30/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eloRanker/eloRankerApp.swift
@@ -0,0 +1,17 @@
+//
+//  eloRankerApp.swift
+//  eloRanker
+//
+//  Created by Rodrigo Sánchez Yuste on 30/4/25.
+//
+
+import SwiftUI
+
+@main
+struct eloRankerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
